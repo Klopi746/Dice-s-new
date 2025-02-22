9c1a52746aba66cb6ab173ed7bf9be852ad071be
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-02-23 00:47:47 +0300

    0.56.1 - continue development of comboFindFunc

--- Assets/Scripts/Players/Player/TestPlayerComboFinder.cs
+++ Assets/Scripts/Players/Player/TestPlayerComboFinder.cs
@@ -1,16 +1,33 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TestPlayerComboFinder : MonoBehaviour
 {
     public Dictionary<string, int> FindAllCombos(Dictionary<int, int> diceValues)
     {
-        string values = "";
+        List<int> values = new List<int>(6);
         foreach (var item in diceValues)
         {
-            values += item.Value.ToString();
+            values.Add(item.Value);
         }
-        Debug.Log(values);
+
+        values.Sort();
+
+        string valuesStr = string.Join("", values);
+
+        int[] valuesCount = new int[6];
+        for (int i = 1; i <= valuesStr.Length; i++)
+        {
+            valuesCount[i - 1] = valuesStr.Count(x => x == i.ToString()[0]);
+        }
+        for (int i = 0; i < valuesCount.Length; i++)
+        {
+            if (valuesCount[i] >= 3)
+            {
+                Debug.Log($"Combo: {string.Concat(Enumerable.Repeat((i + 1).ToString(), valuesCount[i]))}");
+            }
+        }
 
         Dictionary<string, int> foundCombos = new Dictionary<string, int>(){
             { "5", 50 }, { "1", 100 }, { "15", 150 }
