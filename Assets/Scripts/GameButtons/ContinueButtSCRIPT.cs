d3a0b40eb815202d6a09c4390be5649b84cb6afa
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-02-26 20:35:01 +0300

    0.71 - add scripts to new butts

--- Assets/Scripts/GameButtons/ContinueButtSCRIPT.cs
+++ Assets/Scripts/GameButtons/ContinueButtSCRIPT.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ContinueButtSCRIPT : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Button button;
+    public static ContinueButtSCRIPT Instance;
+    private void Awake()
     {
-        
+        Instance = this;
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClicked);
+        button.interactable = false;
+    }
+    private void Start()
+    {
+        GameHandlerSCRIPT.Instance.OnTurnChanged.AddListener(HandleTurnChange);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnButtonClicked()
     {
-        
+        button.interactable = false;
+        PlayerSCRIPT.Instance.ContinuePlay();
+    }
+
+
+    private void HandleTurnChange(bool isPlayerTurn)
+    {
+        //if (isPlayerTurn) button.interactable = true; // Butt is changing in PlayerPapa.GetAndDropCubes();
+    }
+
+
+    public void ChangeButtInteractable(bool value)
+    {
+        button.interactable = value;
     }
 }
--- Assets/Scripts/GameButtons/PassButtSCRIPT.cs
+++ Assets/Scripts/GameButtons/PassButtSCRIPT.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PassButtSCRIPT : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Button button;
+    public static PassButtSCRIPT Instance;
+    private void Awake()
     {
-        
+        Instance = this;
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClicked);
+        button.interactable = false;
+    }
+    private void Start()
+    {
+        GameHandlerSCRIPT.Instance.OnTurnChanged.AddListener(HandleTurnChange);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnButtonClicked()
     {
-        
+        button.interactable = false;
+        LoadSceneManagerSCRIPT.Instance.LoadNewScene();
+    }
+
+
+    private void HandleTurnChange(bool isPlayerTurn)
+    {
+        if (isPlayerTurn) button.interactable = true;
+        else button.interactable = false;
+    }
+
+
+    public void ChangeButtInteractable(bool value)
+    {
+        button.interactable = value;
     }
 }
