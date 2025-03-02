bd1c99dcd1ebef203d3c13620dca19e9ebbfa88b
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-03-02 14:28:26 +0300

    0.89 - change GetRandomValue to get from (0; maxSum)

--- Assets/Scripts/Dices/UnUsualNotNormalDice.cs
+++ Assets/Scripts/Dices/UnUsualNotNormalDice.cs
@@ -5,20 +5,25 @@ public class UnUsualNotNormalDice : DicePapaSCRIPT
     public override void Roll()
     {
         CurrentNumber = GetWeightedRandomFace();
-        
+
         transform.localRotation = Quaternion.Euler(_numberRotations[CurrentNumber - 1]);
         gameObject.SetActive(true);
     }
     private int GetWeightedRandomFace()
     {
-        float randomPoint = Random.value;
         float cumulative = 0f;
-
-
-        for(int i = 0; i < _faceProbabilities.Length; i++)
+        for (int i = 0; i < _faceProbabilities.Length; i++)
         {
             cumulative += _faceProbabilities[i];
-            if(randomPoint <= cumulative)
+        }
+
+
+        float randomPoint = Random.Range(0f, cumulative);
+        cumulative = 0f;
+        for (int i = 0; i < _faceProbabilities.Length; i++)
+        {
+            cumulative += _faceProbabilities[i];
+            if (randomPoint <= cumulative)
             {
                 return i + 1;
             }
